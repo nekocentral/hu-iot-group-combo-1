3fcf8082ac47bf78e9fc51d2644e6944c2e137be
Anvioned <info@anvion.nl>
2020-10-12 10:40:51 +0200

    Small first try
    Added class
    Added requirement

---
+++ IoT.QRCode/generate_qrcode.py
@@ -0,0 +1,11 @@
+'''Genereerd QR code op basis van input parameters'''
+
+import qrcode
+
+class qrcode:
+    '''Class containing all objects of the QR code'''
+
+    def __init__(self):
+        '''Function to initiaize default variables needed
+        to generate a QR code'''
+

b5b17919bf5d96853e18e419c84ff85a2e4b6821
Anvioned <info@anvion.nl>
2020-10-13 16:30:30 +0200

    Small fix

--- IoT.QRCode/fill_values.py
+++ IoT.QRCode/fill_values.py
@@ -10,7 +10,7 @@ fill_database.new_ruimte(4, 'Kroeg')
 fill_database.new_ruimte(5, 'Slaapkamer')
 
 # Deze doen per persoon
-persoons_id = new_person('Ralph', 'van Leeuwen', 1, 1)
+persoons_id = fill_database.new_person('Ralph', 'van Leeuwen', 1, 1)
 fill_database.new_tag(6666, 1, 0, persoons_id)
 fill_database.new_toegang(persoons_id, 1, 1)
 fill_database.new_toegang(persoons_id, 2, 0)
